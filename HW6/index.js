3e291b9447f58eafe910b86ba063abdc8a3ef75e
NastyaTuz <62496991+NastyaTuz@users.noreply.github.com>
2023-06-05 20:51:32 +0200

    Add files via upload

---
+++ HW6/index.js
@@ -0,0 +1,36 @@
+/* Задача 1
+const number = +prompt("Введите число")
+console.log(number * .1)
+*/
+
+/* Задача 2
+const number1 = +prompt("Введите первое число")
+const number2 = +prompt("Введите второе число")
+  if (number1 < number2) {
+    console.log("Наименьшее число: " + number1);
+  } else if (number2 < number1) {
+    console.log("Наименьшее число: " + number2);
+  }
+  */
+
+/* Задача 3
+const number = +prompt("Введите число")
+  if(number < 100) {
+    console.log("Меньше")
+  } else if(number > 100) {
+    console.log("Больше")
+  } else {
+    console.log("Равно")
+  }
+  */
+
+/* Задача 4
+const name = prompt("Введите ваше имя")
+const birth = +prompt("Введите ваш год рождения")
+const age = 2023 - birth
+    if(age >= 18) {
+    console.log(`Hello, ${name}`)
+  } else {
+    console.log(`Hi, ${name}`)
+  }
+*/
